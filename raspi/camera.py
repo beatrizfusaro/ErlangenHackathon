4b8f214470f0a574634b914e864bc6bb07303214
Luke_Remus <l.meysel@gmx.de>
2017-07-01 02:42:57 +0200

    add initial camera module for raspi

---
+++ raspi/camera.py
@@ -0,0 +1,11 @@
+from picamera import PiCamera
+from time import sleep
+import datetime
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(1)
+camera.capture('/home/pi/image_{:%Y-%m-%d_%H:%M:%S}.jpg'.format(datetime.datetime.now()))
+camera.stop_preview()
+
