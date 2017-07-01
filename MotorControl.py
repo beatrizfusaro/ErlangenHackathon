0eaa03bd35c3c1035815a40c9f46b72eba43b8a5
Beatriz Fusaro <beatrizfusaro@gmail.com>
2017-07-01 02:15:13 +0200

    Update MotorControl.py

--- MotorControl.py
+++ MotorControl.py
@@ -12,6 +12,9 @@ class MotorControl:
         self.s = IO.PMW(13, 100)
         t.start(5)
         s.start(5)
+        IO.setup(26,IO.OUT)
+        self.p = IO.PMW(26, 50)
+        p.start(2.5)
         self.r = radius
 
     def move(self, x, y):
@@ -21,3 +24,9 @@ class MotorControl:
        angle = 180.0 * y / 3.14 / r
        duty = float(angle) / 10.0 + 2.5
        s.ChangeDutyCycle(duty)
+       p.ChangeDutyCycle(12.5)
+       time.sleep(1)
+       p.ChangeDutyCycle(2.5)
+       time.sleep(0.5)
+       t.ChangeDutyCycle(5)
+       s.ChangeDutyCycle(5)
