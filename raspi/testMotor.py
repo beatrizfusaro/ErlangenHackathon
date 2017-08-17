5aff733539402864ba93d1edc9d16aa9208d1276
Beatriz Fusaro <beatrizfusaro@gmail.com>
2017-08-17 21:52:57 +0200

    Update testMotor.py

--- raspi/testMotor.py
+++ raspi/testMotor.py
@@ -12,21 +12,18 @@ class MotorControl:
         GPIO.setwarnings(False)             # do not show any warnings
         GPIO.setmode (GPIO.BCM)
                                             # programming the GPGPIO by BCM pin numbers. (like PIN29 as‘GPGPIO5’)
-        GPIO.setup(19,GPIO.OUT)             # initialize GPGPIO19 as an output
-        GPIO.setup(13,GPIO.OUT)
-        GPIO.setup(26,GPIO.OUT)
+        GPIO.setup(17,GPIO.OUT)             # initialize GPGPIO19 as an output
+        GPIO.setup(27,GPIO.OUT)
+        GPIO.setup(22,GPIO.OUT)
 
-        self.t = GPIO.PWM(19, 100)
-        self.s = GPIO.PWM(13, 100)
-        self.p = GPIO.PWM(26, 50)
+        self.t = GPIO.PWM(17, 100)
+        self.s = GPIO.PWM(27, 100)
 
         self.dutycycle_t = 5
         self.dutycycle_s = 5
-        self.dutycycle_p = 7.5
 
         self.t.start(self.dutycycle_t)
         self.s.start(self.dutycycle_s)
-        self.p.start(self.dutycycle_p)
 
     def cleanup_servos(self):
         self.update_dutycycles_s_t(5, 5)
@@ -74,7 +71,7 @@ class MotorControl:
 
 
     def move(self, x, y): #max x and y = 3.14 each
-        angle = 180.0 * x / (3.14)
+        angle = degrees(acos(((x + 4.4) ** 2 - 19.68)/(15.2 * (x + 4.4))))
         print ("Angle X: " +str(angle))
         if angle > 180:
             duty_t = 180 / 10.0 + 2.5
@@ -83,7 +80,7 @@ class MotorControl:
             duty_t = float(angle) / 10.0 + 2.5
             print("Duty X: " + str(duty_t))
 
-        angle = 180.0 * y / 3.14
+        angle = 90 - degrees(acos(((y + 3.7) ** 2 - 13.8)/(12.8 * (y + 3.7))))
         print ("Angle Y: " +str(angle))
         if angle > 180:
             duty_s = 180 / 10.0 + 2.5
@@ -96,9 +93,9 @@ class MotorControl:
         self.update_dutycycles_s_t(duty_s, duty_t)
 
     def press(self):
-        self.p.ChangeDutyCycle(2.5)
+        GPIO.output(22, 1)
         time.sleep(1)
-        self.p.ChangeDutyCycle(7.5)
+        GPIO.output(22, 0)
         time.sleep(0.5)
 try:
 
